878544bf96a0977f613eb59e38aa63280918c2ce
Rodolfo Silva <90969388+Rodolfo-A-Silva@users.noreply.github.com>
2022-07-19 19:53:51 -0300

    ScrollView with CardView
    ScrollView with CardView  in SwiftUI

---
+++ 04-ScrollCardView/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  04-ScrollCardView
+//
+//  Created by rodolfo silva on 19/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            
+            VStack {
+                Image("udemy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    //.padding(-50)
+                    .frame(height: 80)
+                
+                Text("Rodolfo Alves da Silva")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                
+                Text("Quicko, Desenvolvedor y Programador")
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+                
+                Text("139.245 estudantes - 59 cursos - 23.535 resenhas")
+                    .font(.system(.footnote, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.secondary)
+            }
+            
+
+            CardView(imageName: "1", authorName: "Rodolfo Alves da Silva", courseTitle: "Diseño de apps para IOS 13 con Swift UI desde cero", originalPrice: "$199,99", discountPrice: "$10,99")
+            
+            CardView(imageName: "2", authorName: "Ricardo Alberich, Rodolfo Alves da Silva", courseTitle: "Curso completo de videojuegos con unity 2019", originalPrice: "$199,99", discountPrice: "$11,99")
+            
+            CardView(imageName: "3", authorName: "Rodolfo Alves da Silva", courseTitle: "Curso completo de Probabilidad y variable Aleatorias para Machine Learning", originalPrice: "$199,99", discountPrice: "$12,99")
+            
+            CardView(imageName: "4", authorName: "Rodolfo Alves da Silva y Lorenço Valverde", courseTitle: "Resuelve problemas de matemáticas com Sage y  Python", originalPrice: "$99,99", discountPrice: "$10,99")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 04-ScrollCardView/_4_ScrollCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_ScrollCardViewApp.swift
+//  04-ScrollCardView
+//
+//  Created by rodolfo silva on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct _4_ScrollCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
